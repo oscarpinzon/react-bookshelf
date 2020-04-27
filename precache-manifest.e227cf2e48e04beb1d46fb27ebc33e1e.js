56e0bfc5abe83a1ea2750c1deff59fb6f327d6f6
Oscar Pinzon <oskarin31@gmail.com>
2020-04-27 18:54:55 -0500

    Updates

---
+++ precache-manifest.e227cf2e48e04beb1d46fb27ebc33e1e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6fac8d08876a0e5061a7ca2d5b97aeb",
+    "url": "/react-bookshelf/index.html"
+  },
+  {
+    "revision": "faf9bda547f52d393f29",
+    "url": "/react-bookshelf/static/css/main.c77dd72c.chunk.css"
+  },
+  {
+    "revision": "300dccf5375e612f080b",
+    "url": "/react-bookshelf/static/js/2.77c247e6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-bookshelf/static/js/2.77c247e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "faf9bda547f52d393f29",
+    "url": "/react-bookshelf/static/js/main.1732eadc.chunk.js"
+  },
+  {
+    "revision": "2848743a46cf94106c12",
+    "url": "/react-bookshelf/static/js/runtime-main.49270fd0.js"
+  },
+  {
+    "revision": "fe84d99db02d4e650b24505748372c51",
+    "url": "/react-bookshelf/static/media/background.fe84d99d.jpg"
+  }
+]);
